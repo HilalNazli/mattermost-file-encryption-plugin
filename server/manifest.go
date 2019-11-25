5750d84c79f3d0e99a367b9f2de5a69bfca12908
Hilal Ozdemir <nhilalozdemir@gmail.com>
2019-11-25 15:29:48 +0000

    update manifest ID

--- server/manifest.go
+++ server/manifest.go
@@ -6,6 +6,6 @@ var manifest = struct {
 	ID      string
 	Version string
 }{
-	ID:      "com.mattermost.plugin-starter-template",
+	ID:      "com.github.mattermost-file-encryption-plugin",
 	Version: "0.1.0",
 }
