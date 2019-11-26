2e1e87de968a141c50a7a523825da4fad20c709a
Hilal Ozdemir <nhilalozdemir@gmail.com>
2019-11-26 15:40:23 +0000

    decrypt downloaded text file

--- server/plugin.go
+++ server/plugin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/armor"
@@ -80,8 +81,40 @@ func (p *Plugin) FileWillBeRead(c *plugin.Context, file io.Reader) {
 		p.API.LogError(err.Error())
 		return
 	}
-	fmt.Printf(string(data))
 
-	file = strings.NewReader("Lets change the content of the downloaded text file :)")
+	// passphrase will be moved to env variable in the future.
+	passphrase := []byte("myTemporaryPassphrase1")
+
+	packetConfig := &packet.Config{
+		DefaultCipher: packet.CipherAES256,
+	}
+
+	decryptedData, _ := Decrypt(data, passphrase, packetConfig)
+	// fmt.Printf(string(decrypted))
+
+	file = strings.NewReader(string(decryptedData))
+	return
+}
+
+// Decrypt function source: https://play.golang.org/p/vk58yYArMh and https://asecuritysite.com/encryption/go_pgp
+func Decrypt(ciphertext []byte, password []byte, packetConfig *packet.Config) (plaintext []byte, err error) {
+	decbuf := bytes.NewBuffer(ciphertext)
+	armorBlock, _ := armor.Decode(decbuf)
+	failed := false
+	prompt := func(keys []openpgp.Key, symmetric bool) ([]byte, error) {
+		if failed {
+			return nil, errors.New("decryption failed")
+		}
+		failed = true
+		return password, nil
+	}
+	md, err := openpgp.ReadMessage(armorBlock.Body, nil, prompt, packetConfig)
+	if err != nil {
+		return
+	}
+	plaintext, err = ioutil.ReadAll(md.UnverifiedBody)
+	if err != nil {
+		return
+	}
 	return
 }
